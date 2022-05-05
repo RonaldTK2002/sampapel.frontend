4f1dd9655fe3ee1940f4924e30ff6e657a051f74
André Lamego Reis de Castro <andrelamegobh@hotmail.com>
2022-05-05 16:37:31 -0300

    arrumando diretório dos botões

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import './Home.css'
-import Button1 from '../../Components/Buttons/Button 1'
 
 function Home(){
-return(
-    <Button1/>
-)
+
 }
 
 export default Home 
---
+++ src/Pages/Perfil/Perfil.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Perfil.css'
+
+function Perfil(){
+    
+}
+
+
+export default Perfil
