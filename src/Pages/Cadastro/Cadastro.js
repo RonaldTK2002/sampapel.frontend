b2d64dbcf1a047083e480b3d00f9242d49e35b3f
Ronaldo Dutra de Carvalho <ronaldodutra777@gmail.com>
2022-05-05 15:27:18 -0300

    corrigindo node_files

---
+++ src/Pages/Cadastro/Cadastro.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Cadastro.css'
+
+function Cadastro(){
+    
+}
+
+
+export default Cadastro
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import Login from "./Pages/Login";
+import Home from './Pages/Home'
+import Cadastro from './Pages/Cadastro'
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path = '/cadastro' component = {Cadastro}/>
         <Route path="/login" component={Login} />
+        <Route path= '/' component = {()=> <Redirect to ='/login'/>}/>
+        <Route path ='/home' component = {Home}/>
       </Switch>
     </BrowserRouter>
   );
