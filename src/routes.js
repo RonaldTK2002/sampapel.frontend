44491f4d7aa0f713136b79b9694e5745730c68fa
André Lamego Reis de Castro <andrelamegobh@hotmail.com>
2022-05-10 16:13:29 -0300

    Adicionei perfil nas rotas

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import Login from "./Pages/Login";
 import Home from "./Pages/Home";
 import Cadastro from "./Pages/Cadastro";
+import Perfil from "./Pages/Perfil";
 
 function Routes() {
   return (
@@ -11,6 +12,7 @@ function Routes() {
         <Route path="/cadastro" component={Cadastro} />
         <Route path="/login" component={Login} />
         <Route path="/home" component={Home} />
+        <Route path="/perfil" component={Perfil} />
         <Route path="/" component={() => <Redirect to="/login" />} />
       </Switch>
     </BrowserRouter>
